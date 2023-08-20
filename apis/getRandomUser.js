21804a17e64eedc1eb133777efd1027ec17ec66c
big-lark <81342631+big-lark@users.noreply.github.com>
2023-08-20 10:27:30 +0900

    update getUser

--- apis/getRandomUser.js
+++
@@ -1,7 +0,0 @@
-export const getRandomUser = async () => {
-  const result = await fetch("https://randomuser.me/api/", {
-    headers: { Accept: "application/json", "Content-Type": "application/json" },
-    method: "GET",
-  }).then((e) => e.json());
-  return result;
-};
--- controllers/app/main.controller.js
+++ controllers/app/main.controller.js
@@ -39,3 +39,11 @@ export const friendsPerferences = async () => {
   );
   return keyword;
 };
+
+export const getRandomUser = async () => {
+  const result = await fetch("https://randomuser.me/api/", {
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+    method: "GET",
+  }).then((e) => e.json());
+  return result;
+};
